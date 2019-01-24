3902874387b2a118a76faff214637f69467379b3
rodrigosrtz <rodrigosrtz@gmail.com>
2019-01-24 08:04:06 -0200

    Bivariate visualization and create second bad model for baseline

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ df_train.head() #default 5
 df_test.head()
 
 # Looking more accurately at df_train's data
+# View the last line of df_train
+df_train.tail()
+
 # .info prints the summary of a dataframe
 df_train.info()
 
@@ -37,8 +40,27 @@ sns.countplot(x='Survived', data=df_train)
 
 # Creating a bad Predictive model for baseline
 df_test['Survived'] = 0
+df_test.head()
 df_test[['PassengerId', 'Survived']].to_csv('data/predictions/no_survivors.csv', index=False)
 
-# Checking created no_survivors.csv
-df_badmodel = pd.read_csv('data/predictions/no_survivors.csv')
-df_badmodel.head()
+# Using univariate visualization method to see gender variable
+sns.countplot(x='Sex', data=df_train)
+
+# Using bivariate visualization method to check if there is a
+# relationship between gender and target variable
+sns.catplot(x='Survived', col='Sex', kind='count', data=df_train)
+# Looks like women were more likely to survive than man
+
+# Checking more accurately how many men and women Survived
+df_train.groupby(['Sex']).Survived.sum()
+# Calculating the proportion of survivors for each gender
+print(df_train[df_train.Sex == 'female'].Survived.sum()/df_train[df_train.Sex == 'female'].Survived.count())
+print(df_train[df_train.Sex == 'male'].Survived.sum()/df_train[df_train.Sex == 'male'].Survived.count())
+# 74.2% of women survived, while only 18.9% of men survived
+
+# Building second shitty model where all women survived and man didn't
+# Again, just for baseline for future models
+df_test['Survived'] = df_test.Sex == 'female' #Creates 'Survived' column with True for females and False for males
+df_test['Survived'] = df_test.Survived.apply(lambda x: int(x)) #Changes Bool True/False for 1/0
+df_test.head()
+df_test[['PassengerId', 'Survived']].to_csv('data/predictions/women_survived.csv', index=False)
